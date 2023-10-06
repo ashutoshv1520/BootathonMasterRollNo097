23f0766a0700249638f1e48e1b134900d8f3b330
ayushawasthid365 <146088689+ayushawasthid365@users.noreply.github.com>
2023-10-06 10:35:40 +0530

    Update app.ts
    Removed pow lib logic to make it suitable for all plaforms

--- codes/Module 3/Chapter 1/assignments/assignment1/app.ts
+++ codes/Module 3/Chapter 1/assignments/assignment1/app.ts
@@ -1,14 +1,18 @@
 var p: number[] = [];
 var q: number[] = [];
+var qq: number[] = [];
+
 
 //loop from 1 to 20 to store number and its cube.
 for(let i=0;i<20;i++){
     p[i]=(i+1);
     q[i]=Math.pow((i+1),3);
+    qq[i] = (i+1)*(i+1)*(i+1);
 }
 
 //printing heading
 document.getElementById("display").innerHTML+="<br><b>Number: Cubes:</b>";
+
 
 //printing values
 for(let i=0;i<20;i++){
